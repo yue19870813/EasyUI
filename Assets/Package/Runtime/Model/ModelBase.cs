f41ede020f45cb052edaa3e0c506c21ff6b9df70
Yue <yue19870813@163.com>
2023-08-11 09:43:22 +0800

    fix: remove unuse code

--- Assets/Package/Runtime/Model/ModelBase.cs
+++ Assets/Package/Runtime/Model/ModelBase.cs
@@ -11,29 +11,7 @@ namespace EUI
 {
     public class ModelBase : Property
     {
-        // public new void _Init_()
-        // {
-        //     base._Init_();
-        //     // AddChangeWatcher();
-        // }
 
-        // private void AddChangeWatcher()
-        // {
-        //     var t = this.GetType();
-        //     var properties = t.GetProperties();
-        //     foreach (PropertyInfo property in properties)
-        //     {
-        //         var ooAtt = property.GetCustomAttribute<ObserveObjectAttribute>();
-        //         if (ooAtt != null)
-        //         {
-        //             var value = ((ObservableObject)property.GetValue(this));
-        //             value.PropertyChanged += (s, e) => {
-        //                 InvokePropertyChanged(property.Name);
-        //             };
-        //             // Debug.Log(property.Name + " --- " + ((ObservableObject)property.GetValue(this)));
-        //         }
-        //     }
-        // }
         protected new bool OnPropertyChanged<T>(ref T field, T value, [CallerMemberName]string propertyName = null) 
         {
             var flag = base.OnPropertyChanged(ref field, value, propertyName);
