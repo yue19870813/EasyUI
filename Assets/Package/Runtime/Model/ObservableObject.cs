448d6e1208debf0cc5a7c9bfb95af5ef0b611058
Yue <yue19870813@163.com>
2023-08-17 17:00:39 +0800

    feat: add plugins

--- Assets/Package/Runtime/Model/ObservableObject.cs
+++ Assets/Package/Runtime/Model/ObservableObject.cs
@@ -8,37 +8,6 @@ namespace EUI
 {
     public class ObservableObject : Property
     {
-        // public new void _Init_()
-        // {
-        //     AddValueChangeWatcher();
-        //     base._Init_();
-        // }
-
-        // protected new void OnPropertyChanged<T>(ref T field, T value, [CallerMemberName]string propertyName = null) 
-        // {
-        //     base.OnPropertyChanged(ref field, value, propertyName);
-            
-        // }
-
-        // protected void AddValueChangeWatcher()
-        // {
-        //     var t = this.GetType();
-        //     var properties = t.GetProperties();
-        //     foreach (PropertyInfo property in properties)
-        //     {
-        //         var ooAtt = property.GetCustomAttribute<ObserveObjectAttribute>();
-        //         if (ooAtt != null)
-        //         {
-                    
-        //             // Debug.Log("===AddChangeWatcher:" + property.Name + " ---- " + ((ObservableObject)property.GetValue(this)));
-        //         }
-        //     }
-        // }
-
-        // protected ModelBase mModel;
-
-        // protected event PropertyChangedEventHandler MPropertyChanged;
-
         
     }
 }
